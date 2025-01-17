beec5ee82e06f00cb70bc8e2258b51a9393d89dc
BugHunter-X <askzenai@gmail.com>
2025-01-17 20:00:07 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,9 @@
+```javascript
+// pages/index.js
+export default function Home() {
+  return (
+    <div>Hello, world!</div>
+  );
+}
+```
+This code will work fine in Next.js 15, however, adding a dynamic route or changing the way the page is rendered might introduce bugs.
